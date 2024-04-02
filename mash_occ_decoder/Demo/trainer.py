6238de4ee60777df4036e6f470d331f7fb876168
chLi <565353780@qq.com>
2024-04-02 19:12:44 +0800

    best params v1

--- mash_occ_decoder/Demo/trainer.py
+++ mash_occ_decoder/Demo/trainer.py
@@ -7,14 +7,14 @@ def demo():
     model_file_path = None
     dtype = torch.float64
     device = "cuda:0"
-    warm_epoch_step_num = 20
-    warm_epoch_num = 10
-    finetune_step_num = 1000
-    lr = 1e-5
+    warm_epoch_step_num = 400
+    warm_epoch_num = 20
+    finetune_step_num = 100000
+    lr = 1e-4
     weight_decay = 1e-4
     factor = 0.9
-    patience = 10
-    min_lr = 1e-7
+    patience = 40
+    min_lr = 1e-8
     save_result_folder_path = "auto"
     save_log_folder_path = "auto"
 
