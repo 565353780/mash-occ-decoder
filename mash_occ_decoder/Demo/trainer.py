848b2eab289ab0f2dffee529c1bcf8b2b48475d1
chLi <565353780@qq.com>
2024-04-25 23:10:43 +0800

    set to better batch size and lr

--- mash_occ_decoder/Demo/trainer.py
+++ mash_occ_decoder/Demo/trainer.py
@@ -7,17 +7,17 @@ from mash_occ_decoder.Module.trainer import Trainer
 def demo():
     dataset_root_folder_path = "/home/chli/Dataset/"
     batch_size = 16
-    accum_iter = 10
+    accum_iter = 16
     num_workers = 4
     n_qry = 12000
     noise_label_list = ["0_25", "0_025", "0_0025"]
-    model_file_path = "./output/t-v3-1/model_last.pth"
+    model_file_path = "./output/t-v3-2/model_last.pth"
     dtype = torch.float32
     device = "cuda:0"
     warm_epoch_step_num = 100
     warm_epoch_num = 0
     finetune_step_num = 100000000
-    lr = 1e-5
+    lr = 5e-5
     weight_decay = 1e-10
     factor = 0.99
     patience = 10000
