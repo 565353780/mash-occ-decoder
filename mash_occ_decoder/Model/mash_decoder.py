e86839baf9bc4e3a35a781cf018e9856c35aaf1f
chLi <565353780@qq.com>
2024-04-26 22:03:25 +0800

    add mamba model import check to avoid error info

--- mash_occ_decoder/Model/mash_decoder.py
+++ mash_occ_decoder/Model/mash_decoder.py
@@ -4,7 +4,11 @@ from mash_occ_decoder.Model.Decoder.Transformer.v1 import (
 from mash_occ_decoder.Model.Decoder.Transformer.v2 import (
     MashDecoder as TAllEmbed,
 )
-from mash_occ_decoder.Model.Decoder.Mamba.v1 import MashDecoder as MSource
-from mash_occ_decoder.Model.Decoder.Mamba.v2 import MashDecoder as MAllEmbed
+try:
+    from mash_occ_decoder.Model.Decoder.Mamba.v1 import MashDecoder as MSource
+    from mash_occ_decoder.Model.Decoder.Mamba.v2 import MashDecoder as MAllEmbed
+except:
+    print('[ERROR][mash_decoder::import]')
+    print('\t import mamba model failed! current env can only run with transformer models')
 
 MashDecoder = TAllEmbed
