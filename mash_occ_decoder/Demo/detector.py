c46a2e091a61343bb5f2e372e418ba158468fac4
chLi <565353780@qq.com>
2024-12-29 01:23:50 +0800

    finish detect folder

--- mash_occ_decoder/Demo/detector.py
+++ mash_occ_decoder/Demo/detector.py
@@ -4,23 +4,19 @@ sys.path.append("../ma-sh/")
 sys.path.append("../distribution-manage/")
 
 import os
-import torch
-import open3d as o3d
-from shutil import copyfile
 
 from ma_sh.Model.mash import Mash
 from ma_sh.Config.custom_path import toDatasetRootPath
 
-from mash_occ_decoder.Dataset.sdf import SDFDataset
 from mash_occ_decoder.Method.time import getCurrentTime
 from mash_occ_decoder.Module.detector import Detector
 
 
 def demo():
     model_file_path = "../../output/20241227_16:33:30/model_best.pth".replace('../.', '')
-    noise_label = "0_25"
     transformer_id = 'Objaverse_82K'
     device = "cpu"
+    pcd_folder_path = '../../../td-shape-to-vec-set/output/sample/20241228_14:43:22/'.replace('../.', '')
     save_folder_path = './output/recon/' + getCurrentTime() + '/'
 
     os.makedirs(save_folder_path, exist_ok=True)
@@ -28,38 +24,26 @@ def demo():
     dataset_root_folder_path = toDatasetRootPath()
     assert dataset_root_folder_path is not None
 
-    sdf_dataset = SDFDataset(
-        dataset_root_folder_path,
-        "train",
-        noise_label=noise_label,
-        train_percent=0.9,
-    )
-
     detector = Detector(model_file_path, transformer_id, device)
 
-    test_idxs = [
-        1, 2, 3, 4, 5, 6, 7, 8, 9,
-    ]
+    for root, _, files in os.walk(pcd_folder_path):
+        for file in files:
+            if not file.endswith('.npy'):
+                continue
 
-    for i in test_idxs:
-        mash_params_file_path, sdf_file_path = sdf_dataset.paths_list[i]
-        gt_mesh_file_path = sdf_file_path.replace(
-            "manifold_sdf_" + noise_label,
-            "manifold"
-        ).replace(".npy", ".obj")
+            rel_folder_path = os.path.relpath(root, pcd_folder_path)
 
-        print("start export mesh", i, "...")
-        mesh = detector.detectFile(mash_params_file_path)
-        print(mesh)
-        mesh.export(save_folder_path + "recon_" + str(i) + "_mesh.obj")
+            mash_file_path = root + '/' + file
 
-        mash = Mash.fromParamsFile(mash_params_file_path, device=device)
-        mash_points = torch.cat(mash.toSamplePoints()[:2], dim=0).detach().clone().cpu().numpy()
-        pcd = o3d.geometry.PointCloud()
-        pcd.points = o3d.utility.Vector3dVector(mash_points)
-        o3d.io.write_point_cloud(
-            save_folder_path + "mash_" + str(i) + "_pcd.ply", pcd, write_ascii=True
-        )
+            save_mesh_folder_path = save_folder_path + rel_folder_path + '/'
+            os.makedirs(save_mesh_folder_path, exist_ok=True)
 
-        copyfile(gt_mesh_file_path, save_folder_path + "gt_" + str(i) + "_mesh.obj")
+            save_mesh_file_path = save_mesh_folder_path + file.replace('mash', 'mesh').replace('.npy', '.obj')
+
+            print("start export mesh for mash " + file + "...")
+            mesh = detector.detectFile(mash_file_path)
+            print(mesh)
+            mesh.export(save_mesh_file_path)
+
+            Mash.fromParamsFile(mash_file_path, device=device).saveAsPcdFile(save_mesh_folder_path + file.replace('mash', 'mash_pcd').replace('.npy', '.ply'))
     return True
