bcce4ee69e9fc8e61e0688493e49463996fe2f65
chLi <565353780@qq.com>
2025-01-08 23:47:09 +0800

    remove archive setup commands

--- dev_setup.sh
+++ dev_setup.sh
@@ -20,13 +20,4 @@ pip install -U PyMCubes
 cd ../light-field-distance
 python setup.py install
 
-cd ../aro-net
-
 pip install -U torch torchvision torchaudio
-# pip install torch==2.2.2 torchvision==0.17.2 torchaudio==2.2.2 --index-url https://download.pytorch.org/whl/cu121
-
-python setup.py build_ext --inplace
-
-mkdir ssl
-cd ssl
-openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 365 -nodes
